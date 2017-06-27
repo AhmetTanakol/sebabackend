624ad790258b365d113b2a3dd5e328df8de69052
blank <x.or@icloud.com>
2017-06-27 16:43:17 +0200

    added the database admin login as comment to config.js

--- config/config.js
+++ config/config.js
@@ -5,10 +5,16 @@ Config.auth = {};
 
 Config.db.user = 'dbAdmin'
 Config.db.pass = 'onlythedatabaseinscope'
+//admin:
+// user: admin
+// pass: hedoeseverything
+
+
 //todo plaintext password
 
 Config.db.host = 'pcjc5ikfd431jnvz.myfritz.net:60420';
 Config.db.name = 'WorkForceDB';
+
 
 // Use environment defined port or 3000
 Config.app.port = process.env.PORT || 3000;
