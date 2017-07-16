8a18bf08270c4ac111a75f732b08495e5172b076
jesseint <jesse.int@gmail.com>
2017-07-16 22:43:14 +0200

    Add update resume with experiences and certificates

--- certificate/certificateController.js
+++ certificate/certificateController.js
@@ -1,1 +1,15 @@
 var Certificate = require('./certificateSchema');
+
+// Create endpoint /api/certificates/:refugee_id for GET
+module.exports.getCertificatesByRefugeeId = function(req, res) {
+    // Use the Certificate model to find a specific Certificate based on refugee_id
+	var myquery = { refugee: req.params.refugee_id }
+    Certificate.find(myquery, function(err, cert) {
+        if (err) {
+            res.status(500).send(err)
+            return;
+        };
+
+        res.json(cert);
+    });
+};
